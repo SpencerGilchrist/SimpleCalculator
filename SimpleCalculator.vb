18c2a6288b9600a8a87342a6f4bf09e33189c0c2
Gilcspen@gmail.com <Gilcspen@gmail.com>
2021-01-22 08:37:39 -0700

    Add project files.

---
+++ SimpleCalculator.vb
@@ -0,0 +1,69 @@
+﻿'Spencer Gilchrist
+'RCET0265
+'Spring 2021
+'Simple Calculator
+
+Module SimpleCalculator
+
+    Sub Main()
+
+        Dim FirstNumber As String
+        Dim SecondNumber As String
+Line1:
+        Console.WriteLine("Enter a Number!")
+        FirstNumber = Console.ReadLine()
+        Console.WriteLine("Enter another number!")
+        SecondNumber = Console.ReadLine()
+
+        Console.WriteLine("Please Choose a funtion.")
+        System.Threading.Thread.Sleep(1500)
+        Console.WriteLine("Q for add. W for subtract. E for divide. R for multiply.")
+
+        Select Case Console.ReadKey.Key
+            Case ConsoleKey.Q
+                Console.Clear()
+                Console.WriteLine(Int(FirstNumber) + Int(SecondNumber))
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Press enter")
+                Console.ReadLine()
+
+            Case ConsoleKey.W
+                Console.Clear()
+                Console.WriteLine(Int(FirstNumber) - Int(SecondNumber))
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Press enter")
+                Console.ReadLine()
+
+            Case ConsoleKey.E
+                Console.Clear()
+                Console.WriteLine(Int(FirstNumber) / Int(SecondNumber))
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Press enter")
+                Console.ReadLine()
+
+            Case ConsoleKey.R
+                Console.Clear()
+                Console.WriteLine(Int(FirstNumber) * Int(SecondNumber))
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Press enter")
+                Console.ReadLine()
+
+        End Select
+
+        Console.WriteLine("Would you Like to keep using the calculator or Exit?")
+        System.Threading.Thread.Sleep(1500)
+        Console.WriteLine("Press Y for Yes or N for No.")
+
+        Select Case Console.ReadKey.Key
+
+            Case ConsoleKey.Y
+                Console.Clear()
+                GoTo Line1
+
+            Case ConsoleKey.N
+                Console.Clear()
+        End Select
+
+    End Sub
+
+End Module
